6cbc267f04307b63f1c65cc7ec348c2aecfd07e2
OliverDoan <doanvietthuan1005@gmail.com>
2023-12-04 16:56:09 +0700

    feat (server): Hash passwprd

--- src/controllers/authenticationController.ts
+++ src/controllers/authenticationController.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { UserModel } from '../models/userModel';
+import bcrypt from 'bcrypt';
 
 const authenticationController = {
   register: async (req: express.Request, res: express.Response) => {
@@ -11,9 +12,16 @@ const authenticationController = {
       });
 
     const user = await UserModel.findOne({ email });
+
     if (user) throw new Error('User has existed');
     else {
-      const newUser = await UserModel.create(req.body);
+      const passwordHashed = await bcrypt.hash(password, 10);
+
+      const newUser = await UserModel.create({
+        ...req.body,
+        password: passwordHashed,
+      });
+
       return res.status(200).json({
         sucess: newUser ? true : false,
         mes: newUser ? 'Register is successfully. Please go login~' : 'Something went wrong',
